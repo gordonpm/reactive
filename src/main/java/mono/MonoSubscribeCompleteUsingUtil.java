8c2d950d3226bc4267a72e1f2894e8ade3b47f4e
gordonpm <gordonm0815@gmail.com>
2022-09-14 21:53:01 -0700

    Refactored using util

---
+++ src/main/java/mono/MonoSubscribeCompleteUsingUtil.java
@@ -0,0 +1,15 @@
+package mono;
+
+import mono.utils.Util;
+import reactor.core.publisher.Mono;
+
+public class MonoSubscribeCompleteUsingUtil {
+    public static void main(String[] args) {
+        Mono<String> mono = Mono.just("bicycle");
+        mono.subscribe(
+                Util.onNext(),
+                Util.onError(),
+                Util.onComplete()
+        );
+    }
+}
