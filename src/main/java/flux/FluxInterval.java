c626d0bd8be969e6a84da04fba72b7c0fa873f1e
gordonpm <gordonm0815@gmail.com>
2022-09-26 19:58:26 -0700

    Flux interval

---
+++ src/main/java/flux/FluxInterval.java
@@ -0,0 +1,17 @@
+package flux;
+
+import reactor.core.publisher.Flux;
+import utils.Util;
+
+import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
+
+public class FluxInterval {
+    public static void main(String[] args) {
+        Flux.interval(Duration.ofSeconds(1))
+                .take(5)
+                .log()
+                .subscribe(Util.onNext());
+        Util.sleepSeconds(10);
+    }
+}
