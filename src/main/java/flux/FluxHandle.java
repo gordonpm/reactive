ef2784bd1f2854a2cb4c644c9bc3f401b8e313b7
gordonpm <gordonm0815@gmail.com>
2022-10-12 20:39:26 -0700

    Flux with handle

---
+++ src/main/java/flux/FluxHandle.java
@@ -0,0 +1,21 @@
+package flux;
+
+import reactor.core.publisher.Flux;
+import utils.Util;
+
+public class FluxHandle {
+
+    public static void main(String[] args) {
+        Flux.range(1, 20)
+                .handle((integer, synchronousSink) -> {
+                    if (integer == 7) {
+                        synchronousSink.complete();
+                    }
+                    else {
+                        synchronousSink.next(integer);
+                    }
+                })
+                .subscribe(Util.subscriber());
+    }
+
+}
