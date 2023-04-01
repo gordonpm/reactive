3f516b5765e829bd358de6277ab898f8225a0e90
gordonpm <gordonm0815@gmail.com>
2023-03-31 21:18:10 -0700

    Flux with Transform

---
+++ src/main/java/flux/FluxTransform.java
@@ -0,0 +1,28 @@
+package flux;
+
+import reactor.core.publisher.Flux;
+import utils.Util;
+
+import java.util.Locale;
+import java.util.function.Function;
+
+public class FluxTransform {
+
+    public static void main(String[] args) {
+        getPerson()
+                .transform(applyFilterMap())
+                .subscribe(Util.subscriber());
+    }
+
+    public static Flux<Person> getPerson() {
+        return Flux.range(1, 10)
+                .map(i -> new Person());
+    }
+
+    public static Function<Flux<Person>, Flux<Person>> applyFilterMap() {
+        return flux -> flux
+                .filter(p -> p.getAge() > 10)
+                .doOnNext(p -> p.setName(p.getName().toUpperCase()))
+                .doOnDiscard(Person.class, p -> System.out.println("Not allowing: " + p));
+    }
+}
