1de1891d542ece1354e2e9dafca13e58b48017ec
gordonpm <gordonm0815@gmail.com>
2023-04-01 18:46:34 -0700

    Flux with SwitchOnFirst

---
+++ src/main/java/flux/FluxSwitchOnFirst.java
@@ -0,0 +1,29 @@
+package flux;
+
+import reactor.core.publisher.Flux;
+import utils.Util;
+
+import java.util.function.Function;
+
+public class FluxSwitchOnFirst {
+
+    public static void main(String[] args) {
+        getPerson()
+                .switchOnFirst((signal, personFlux) -> {
+                    return signal.isOnNext() && signal.get().getAge() > 10 ? personFlux : applyFilterMap().apply(personFlux);
+                })
+                .subscribe(Util.subscriber());
+    }
+
+    public static Flux<Person> getPerson() {
+        return Flux.range(1, 10)
+                .map(i -> new Person());
+    }
+
+    public static Function<Flux<Person>, Flux<Person>> applyFilterMap() {
+        return flux -> flux
+                .filter(p -> p.getAge() > 10)
+                .doOnNext(p -> p.setName(p.getName().toUpperCase()))
+                .doOnDiscard(Person.class, p -> System.out.println("Not allowing: " + p));
+    }
+}
