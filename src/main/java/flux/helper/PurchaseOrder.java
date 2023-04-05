bc6e3138839c494dcd9ba56948f3e1e99cb258f0
gordonpm <gordonm0815@gmail.com>
2023-04-04 20:36:36 -0700

    supporting classes for FlatMap example

---
+++ src/main/java/flux/helper/PurchaseOrder.java
@@ -0,0 +1,20 @@
+package flux.helper;
+
+import lombok.Data;
+import lombok.ToString;
+import utils.Util;
+
+@Data
+@ToString
+public class PurchaseOrder {
+
+    private String item;
+    private String price;
+    private int userId;
+
+    public PurchaseOrder(int userId) {
+        this.userId = userId;
+        this.item = Util.faker().commerce().productName();
+        this.price = Util.faker().commerce().price();
+    }
+}
---
+++ src/main/java/flux/helper/User.java
@@ -0,0 +1,17 @@
+package flux.helper;
+
+import lombok.Data;
+import lombok.ToString;
+import utils.Util;
+
+@Data
+@ToString
+public class User {
+    private int userId;
+    private String name;
+
+    public User(int userId) {
+        this.userId = userId;
+        this.name = Util.faker().name().fullName();
+    }
+}
