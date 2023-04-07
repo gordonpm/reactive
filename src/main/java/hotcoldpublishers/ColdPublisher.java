0f17f8cf09bb0eef6272aaf40ea2339b9b0dd703
gordonpm <gordonm0815@gmail.com>
2023-04-06 21:38:56 -0700

    Cold publisher

---
+++ src/main/java/hotcoldpublishers/ColdPublisher.java
@@ -0,0 +1,35 @@
+package hotcoldpublishers;
+
+import reactor.core.publisher.Flux;
+import utils.Util;
+
+import java.time.Duration;
+import java.util.stream.Stream;
+
+public class ColdPublisher {
+
+    public static void main(String[] args) {
+        Flux<String> movieStream = Flux.fromStream(() -> getMovie())
+                .delayElements(Duration.ofSeconds(2));
+
+        movieStream.subscribe(Util.subscriber("Sam"));
+        Util.sleepSeconds(5);
+        movieStream.subscribe(Util.subscriber("Mike"));
+
+        Util.sleepSeconds(60);
+
+    }
+
+    private static Stream<String> getMovie() {
+        System.out.println("Got the movie streaming req");
+        return Stream.of(
+                "Scene 1",
+                "Scene 2",
+                "Scene 3",
+                "Scene 4",
+                "Scene 5",
+                "Scene 6",
+                "Scene 7"
+        );
+    }
+}
